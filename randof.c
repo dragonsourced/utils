47f8cc47d27e825ab76e82ffa0027b6d3b559f90
dragonsourced <dragonsourced@protonmail.com>
2020-02-13 21:44:28 -0500

    Fool that I am, I forgot to add this.

--- randof.c
+++ randof.c
@@ -8,7 +8,7 @@ int main(int argc, char *argv[])
 	int i;
 
 	if (argc < 2) {
-		printf("Usage: %s [OPT]...");
+		printf("Usage: %s [OPT]...", argv[0]);
 		puts("");
 		puts("Display a random OPT.");
 		return 1;
