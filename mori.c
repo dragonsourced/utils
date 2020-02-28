2a1227465f6c33120bec77b96090c4e0e2ec5acb
dragonsourced <dragonsourced@protonmail.com>
2020-02-27 21:05:55 -0500

    Add weeks counter

--- mori.c
+++ mori.c
@@ -5,6 +5,7 @@
 #define MINUTE 60
 #define HOUR   (60 * MINUTE)
 #define DAY    (24 * HOUR)
+#define WEEK   (7 * DAY)
 #define YEAR   (365 * DAY)
 
 #define LIFESPAN 80
@@ -31,20 +32,21 @@ int main(int argc, char *argv[])
 	time(&current);
 	time_t remaining = death_time - current;
 
-	int days, hours, minutes, seconds;
+	int weeks, days, hours, minutes, seconds;
 	long double used, total, left;
 
 	total = death_time - birth_time;
 	left  = remaining;
 	used  = total - left;
 
+	remaining -= (weeks    = (remaining / WEEK))   * WEEK;
 	remaining -= (days     = (remaining / DAY))    * DAY;
 	remaining -= (hours    = (remaining / HOUR))   * HOUR;
 	remaining -= (minutes  = (remaining / MINUTE)) * MINUTE;
 	seconds = remaining;
 
 	if (argc == 1)
-		printf("%d:%d:%d:%d\n", days, hours, minutes, seconds);
+		printf("%d:%d:%d:%d:%d\n", weeks, days, hours, minutes, seconds);
 	else if (argc == 2 && !strcmp(argv[1], "%left"))
 		printf("%Lf\n", left / (total / 100));
 	else if (argc == 2 && !strcmp(argv[1], "%used"))
